5c6a1afb944dfc22c893948385ab8b3e3e1c26e0
Stefan Hendricks <stefan.hendricks@awi.de>
2017-09-11 11:59:59 +0200

    add matplotlib stype default to pysimre initialization

--- pysimre/__init__.py
+++ pysimre/__init__.py
@@ -1,5 +1,13 @@
 import os
+import matplotlib as mpl
 
 # The local path of the module (e.g. for retrieving resources)
 local_src_path = os.path.split(os.path.abspath(__file__))[0]
 local_path = os.sep.join(local_src_path.split(os.sep)[:-1])
+
+
+# Matplotlib default settings
+mpl.rcParams['font.sans-serif'] = "arial"
+for target in ["xtick.color", "ytick.color", "axes.edgecolor",
+               "axes.labelcolor"]:
+    mpl.rcParams[target] = "#4b4b4d"
