3752f4a105afbefa4f6f0b7d28866d53252235eb
Stefan Hendricks <stefan.hendricks@awi.de>
2017-12-20 15:33:17 +0100

    region definition
    Added a fixed filename for the region definition file

--- pysimre/__init__.py
+++ pysimre/__init__.py
@@ -5,6 +5,7 @@ import matplotlib as mpl
 local_src_path = os.path.split(os.path.abspath(__file__))[0]
 local_path = os.sep.join(local_src_path.split(os.sep)[:-1])
 LOCAL_PATH_RESOURCES = os.path.join(local_path, "resources")
+REGION_DEF_FILENAME = r"simre_region_definition.yaml"
 
 # Matplotlib default settings
 mpl.rcParams['font.sans-serif'] = "arial"
